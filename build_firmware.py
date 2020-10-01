d1d743e46cb8023042c8cf91b2ef2132eed5dd08
shayy <shay.yosef@forescout.com>
2020-10-01 14:17:44 +0300

    test

--- build_firmware.py
+++ build_firmware.py
@@ -15,15 +15,17 @@ def parse_args():
         usage()
         sys.exit(2)
 
-    for current_argument, current_value in arguments:
-        if current_argument in "-h":
+    for flag in range(len(arguments)):
+        if arguments[flag][0] in "-h":
             usage()
-            sys.exit(2)
-        elif current_argument in "-o":
-            output_filename = current_value
-        elif current_argument in "-f":
-            files.append(current_value)
+        if arguments[flag][0] in "-o":
+            output_filename = arguments[flag][1]
+        if arguments[flag][0] in "-f":
+            files.append(arguments[flag][1])
 
+
+def add_file([]):
+    pass
 
 def usage():
     print("Usage: build_firmware.py -o output_filename -f file1 -f file2\n")
